ae6986ba39edf90ca1c735319d89e0805570f4cc
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2020-06-14 19:00:09 +0700

    first

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('.btn-menu').click(function(){
+		$('#menu-mobile').toggleClass('nyala');
+		$('.brand').toggleClass('nyalabrand');
+		$('#side-nav').toggleClass('nyalaside');
+	});
+});
