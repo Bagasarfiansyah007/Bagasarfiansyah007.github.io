8830a38f0b75ad4396e2d649581576ac19556c46
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2021-04-20 04:56:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.btn-menu').click(function(){
+		$('#menu-nav').toggleClass('active');
+        $('h1').toggleClass('active');
+        $('p').toggleClass('active');
+        $('img').toggleClass('active');
+        $(".bar1").toggleClass("active2");
+        $(".bar2").toggleClass("active2");
+	});
+});
+
+$(document).ready(function(){
+    $(".btn-menu").hover(function(){
+        $(".bar1").toggleClass("active");
+        $(".bar2").toggleClass("active");
+    });
+});
