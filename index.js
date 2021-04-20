87aff6bebead1b54b186aa46455786a92a64936d
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2021-04-21 00:39:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+	$('.btn-menu2').click(function(){
+		$('#menu-nav').toggleClass('active');
+        $('h1').toggleClass('active');
+        $('p').toggleClass('active');
+        $('img').toggleClass('active');
+        $(".bar1").toggleClass("active2");
+        $(".bar2").toggleClass("active2");
+        $(".btn-menu2").toggleClass("active");
+	});
+});
+
+$(document).ready(function(){
 	$('.btn-menu').click(function(){
 		$('#menu-nav').toggleClass('active');
         $('h1').toggleClass('active');
@@ -11,6 +23,13 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $(".btn-menu").hover(function(){
+        $(".bar1").toggleClass("active");
+        $(".bar2").toggleClass("active");
+    });
+});
+
+$(document).ready(function(){
+    $(".btn-menu2").hover(function(){
         $(".bar1").toggleClass("active");
         $(".bar2").toggleClass("active");
     });
