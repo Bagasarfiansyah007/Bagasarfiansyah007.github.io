afe4fb9acf87f4ee6b435d0f11939011a945bf2d
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2021-04-22 00:27:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function delay(n) {
 
 function pageTransition() {
     var tl = gsap.timeline();
-    var t2 = gsap.timeline();
     tl.to(".loading-screen", {
         duration: 1.2,
         width: "100%",
@@ -24,22 +23,8 @@ function pageTransition() {
         ease: "Expo.easeInOut",
         delay: 0.3,
     });
-    t2.to(".loading-screen h1", {
-        duration: 1.2,
-        width: "100%",
-        left: "0%",
-        ease: "Expo.easeInOut",
-    });
-
-    t2.to(".loading-screen h1", {
-        duration: 1,
-        width: "100%",
-        left: "100%",
-        ease: "Expo.easeInOut",
-        delay: 0.3,
-    });
+    
     tl.set(".loading-screen", { left: "-100%" });
-    t2.set(".loading-screen h1", { left: "-100%" });
 }
 
 function contentAnimation() {
